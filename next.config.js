99c8326825d3f8cd7bc0db325e3ae4fba303d50a
MR Ferry <ferferfx@gmail.com>
2024-05-07 05:09:13 +0700

    test dsg

---
+++ next.config.js
@@ -0,0 +1,12 @@
+/************************
+ * Made by [MR Ferry™]  *
+ * on Mei 2024          *
+ ************************/
+const nextConfig = {
+  experimental: {
+    nftTracing: true,
+  },
+}
+
+export default nextConfig
+
