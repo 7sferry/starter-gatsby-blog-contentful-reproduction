59afb1914647a8298af86c6b5934bbb89ea6adac
MR Ferry <ferferfx@gmail.com>
2024-05-07 04:31:58 +0700

    test dsg

--- gatsby-config.ts
+++ gatsby-config.ts
@@ -94,20 +94,20 @@ const gatsbyConfig: GatsbyConfig = {
               noInlineHighlight: false,
             },
           },
-          {
-            resolve: `gatsby-remark-images-contentful`,
-            options: {
-              // It's important to specify the maxWidth (in pixels) of
-              // the content container as this plugin uses this as the
-              // base for generating different widths of each image.
-              maxWidth: 1200,
-              linkImagesToOriginal: false,
-              withWebp: true,
-              showCaptions: true,
-              backgroundColor: 'transparent',
-              loading: 'lazy',
-            },
-          },
+          // {
+          //   resolve: `gatsby-remark-images-contentful`,
+          //   options: {
+          //     // It's important to specify the maxWidth (in pixels) of
+          //     // the content container as this plugin uses this as the
+          //     // base for generating different widths of each image.
+          //     maxWidth: 1200,
+          //     linkImagesToOriginal: false,
+          //     withWebp: true,
+          //     showCaptions: true,
+          //     backgroundColor: 'transparent',
+          //     loading: 'lazy',
+          //   },
+          // },
         ],
       },
     },
